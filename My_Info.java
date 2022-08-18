43f018e841fb0ff30684dcbc91201e6bcede2574
akihiro24 <111472710+akihiro24@users.noreply.github.com>
2022-08-18 11:13:36 +0800

    Add files via upload

---
+++ My_Info.java
@@ -0,0 +1,41 @@
+public class My_Info {
+    public static void main(String[] args) {
+
+        //VARIABLES
+        String alaga_ko = "Gigi";
+
+        // int - stores integers (whole numbers), without decimals, such as 123 or -123
+        int myAge = 18;
+        int myFavoriteNumber = 17;
+
+        // float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+        float myGradeInClass = 4.00f;
+
+        // char - stores single characters, such as 'a' or 'B'. Char values are
+        // surrounded by single quotes
+        char myMiddleInitial = 'D';
+
+        // boolean - stores values with two states: true or false
+        final boolean iLoveJhocel = true;
+        boolean iAm5FeetTall = true;
+
+        // String - stores text, such as "Hello". String values are surrounded by double
+        // quotes
+        String myFirstName = "Railey";
+
+        // If you don't want others (or yourself) to overwrite existing values, use the
+        // final keyword (this will declare the variable as "final" or "constant", which
+        // means unchangeable and read-only)
+        final String myLastName = "Dela Vina";
+
+        System.out.println(alaga_ko);
+        System.out.println(myFavoriteNumber);
+        System.out.println(myAge);
+        System.out.println(myGradeInClass);
+        System.out.println(myMiddleInitial);
+        System.out.println(iLoveJhocel);
+        System.out.println(iAm5FeetTall);
+        System.out.println(myFirstName);
+        System.out.println(myLastName);
+    }
+}
