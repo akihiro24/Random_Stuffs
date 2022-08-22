92313f46f190086604c15978e6da748e349b3e54
akihiro24 <111472710+akihiro24@users.noreply.github.com>
2022-08-22 11:30:12 +0800

    Add files via upload

--- Characters/src/App.java
+++ Characters/src/App.java
@@ -1,49 +1,16 @@
 public class App {
     public static void main(String[] args) {
 
-        Character knight = new Character();
+        
 
-        knight.strength = 10;
-        knight.agility = 5;
-        knight.intelligence = 0;
-        knight.name = "Abaddon";
-        knight.sayMyName();
-        knight.sayMyStrength();
-        knight.sayMyAgility();
-        knight.sayMyIntelligence();
+        Character knight = new Character(8,8,8,"Abaddon");
+        
+        Character mage = new Character(8,8,8,"Akasha");
+      
+        Character thief = new Character(8,8,8,"Gondar");
 
-        Character mage = new Character();
+        new Character(8,8,8,"Mirana");
 
-        mage.strength = 0;
-        mage.agility = 5;
-        mage.intelligence = 10;
-        mage.name = "Akasha";
-        mage.sayMyName();
-        mage.sayMyStrength();
-        mage.sayMyAgility();
-        mage.sayMyIntelligence();
-
-        Character thief = new Character();
-
-        thief.strength = 5;
-        thief.agility = 10;
-        thief.intelligence = 0;
-        thief.name = "Gondar";
-        thief.sayMyName();
-        thief.sayMyStrength();
-        thief.sayMyAgility();
-        thief.sayMyIntelligence();
-
-        Character archer = new Character();
-
-        archer.strength = 5;
-        archer.agility = 5;
-        archer.intelligence = 5;
-        archer.name = "Mirana";
-        archer.sayMyName();
-        archer.sayMyStrength();
-        archer.sayMyAgility();
-        archer.sayMyAgility();
-
+       
     }
 }
--- Characters/src/Character.java
+++ Characters/src/Character.java
@@ -19,4 +19,27 @@ public class Character {
     public void sayMyIntelligence() {
         System.out.println("My intelligence is " + intelligence);
     }
+
+    // public class Character {
+    //     int strength;
+    //     int agility;
+    //     int intelligence;
+    //     String name;
+    
+    //     public void sayMyName() {
+    //         System.out.println("Hello I am " + name);
+    //     }
+        
+    //     public void sayMyStrength() {
+    //         System.out.println("My strength is " + strength);
+    //     }
+        
+    //     public Character(int strength, int agility, int intelligence, String name)
+    //     {
+    //         strength = this.strength;
+    //         agility = this.agility;
+    //         intelligence = this.intelligence;
+    //         name = this.name;
+    //     }
+    
 }
