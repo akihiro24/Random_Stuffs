be333ab1aafcd97b508d10ce66e2e90b91be4002
akihiro24 <111472710+akihiro24@users.noreply.github.com>
2022-08-22 22:53:28 +0800

    Update App.java

--- Characters/src/App.java
+++ Characters/src/App.java
@@ -1,16 +1,35 @@
 public class App {
     public static void main(String[] args) {
 
+        Character knight = new Character();
+
+        knight.strength = 10;
+        knight.agility = 5;
+        knight.intelligence = 0;
+        knight.name = "Chen";
+        knight.sayMyName();
+
+        Character mage = new Character();
+
+        mage.strength = 0;
+        mage.agility = 5;
+        mage.intelligence = 10;
+        mage.name = "Akasha";
+        mage.sayMyName();
+
+        Character thief = new Character();
+
+        thief.strength = 5;
+        thief.agility = 10;
+        thief.intelligence = 0;
+        thief.name = "Gondar";
+        thief.sayMyName();
         
-
-        Character knight = new Character(8,8,8,"Abaddon");
-        
-        Character mage = new Character(8,8,8,"Akasha");
-      
-        Character thief = new Character(8,8,8,"Gondar");
-
-        new Character(8,8,8,"Mirana");
-
-       
+        Character archer = new Character();
+        archer.strength = 5;
+        archer.agility = 5;
+        archer.intelligence = 5;
+        archer.name = "Mirana";
+        archer.sayMyName();
     }
 }
